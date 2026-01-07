7d6b7063236cb7e493c6ef2b34e9a3da6c549df1
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-01-07 14:26:24 -0300

    a bugfix

--- usr/lumin/fix.sh
+++ usr/lumin/fix.sh
@@ -7,15 +7,16 @@ sudo rm -rf ~/container/LuminOS
 cd ~/container
 sudo apt update && sudo apt install git -y
 git clone https://github.com/nixxlte/LuminOS
-cd ~/container/LuminOS
+cd ~/container/LuminOS/Root
 cp .bashrc ~/.bashrc
 source ~/.bashrc
-cd ..
+cd ~/container
 ls
 git rm -rf ~/container/LuminCORE
 git clone https://github.com/nixxlte/LuminCORE
 cd LuminCORE/System/Modifications
 cp extensions.md ~/container
+cd .. && cd ..
 mkdir /home/luminos
 cp -r System/Modifications/* /home/luminos
 
