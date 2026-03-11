f5c03d1226c679d319845ab4048a41daa132ccd1
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-03-11 15:45:03 -0300

    T64

--- usr/lumin/update.sh
+++ usr/lumin/update.sh
@@ -3,7 +3,7 @@
 # This is a first version of a kind of Rolling Relase updater, inside of a LTS base
 # you can run this shell script by simply type "sudo lumin update" (in a not distant future =3)
 
-# Code by NyanRay64 =3
+# Code by imNyan.T64 =3
 
 echo "Updating LuminOS"
 
